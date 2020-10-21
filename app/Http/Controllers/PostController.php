0a0c00993edabc119238c4360a31e70cea9da3a4
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-10-21 11:30:12 +0200

    Posts props in vue component (w/ Vue.js and Inertia.js)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Posts');
+        return Inertia::render('Posts', [
+            'posts' => Post::all()
+        ]);
     }
 }
--- resources/js/Pages/Posts.vue
+++ resources/js/Pages/Posts.vue
@@ -1,25 +1,29 @@
 <template>
-  <app-layout>
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Posts</h2>
-    </template>
+	<app-layout>
+		<template #header>
+			<h2 class="font-semibold text-xl text-gray-800 leading-tight">Posts</h2>
+		</template>
 
-    <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          <p>Hello World</p>
-        </div>
-      </div>
-    </div>
-  </app-layout>
+		<div class="py-12">
+			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+				<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+					<show-posts :posts="posts" />
+				</div>
+			</div>
+		</div>
+	</app-layout>
 </template>
 
 <script>
 import AppLayout from "./../Layouts/AppLayout";
+import ShowPosts from "./Components/ShowPosts";
 
 export default {
-  components: {
-    AppLayout,
-  },
+	props: ["posts"],
+
+	components: {
+		AppLayout,
+		ShowPosts,
+	},
 };
 </script>
